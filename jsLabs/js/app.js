766b46797ec4f62332d05e66cc6e7f7c94c5eae0
rashrayhan <rashrayhan@gmail.com>
2019-11-05 04:55:06 -0600

    'completeLab'

--- jsLabs/js/app.js
+++ jsLabs/js/app.js
@@ -77,3 +77,55 @@ function findLongestWord (words) {
 }
 
 console.log(findLongestWord(['The', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']))
+
+// 7. function filterLongWords()
+function filterLongWords (words, i) {
+  return words.filter(e => e.length > i)
+}
+var wor = ['The', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']
+console.log(filterLongWords(wor, 4))
+
+// 8. Array.reduce(…) function, re-implement your functions, sum(…) and multiply(…)
+const reducer = (accumulator, currentValue) => accumulator + currentValue
+const reducerMultiple = (accumulator, currentValue) => accumulator * currentValue
+
+function sumMultiplyFunctional (numbers) {
+  var sum = numbers.reduce(reducer)
+  var multiply = numbers.reduce(reducerMultiple)
+  return 'sum of values: ' + sum + '\n\nmultiple of values: ' + multiply
+}
+console.log(sumMultiplyFunctional([1, 2, 3, 4]))
+
+// 9. function named findSecondBiggest
+function findSecondBiggest (numbers) {
+  var biggest, nextBiggest
+  biggest = nextBiggest = 0
+  for (var i = 0; i < numbers.length; i++) {
+    if (biggest < numbers[i]) {
+      nextBiggest = biggest
+      biggest = numbers[i]
+    } else if (nextBiggest < numbers[i] && numbers !== biggest) {
+      nextBiggest = numbers[i]
+    }
+  }
+  return nextBiggest
+}
+
+console.log(findSecondBiggest([18, 4, 3, 5, 17]))
+
+// 10. function printFibo
+function printFibo (len, start1, start2) {
+  return !len ? start1 + start2 : printFibo(--len, start1, start2 + start1)
+}
+
+console.log(printFibo(3, 0, 1))
+
+// 12. Clock
+function startTime () {
+  var today = new Date()
+  var year = today.getFullYear()
+  var month = today.getMonth() + 1
+  var day = today.getDate()
+  var h = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+  document.getElementById('clock').innerHTML = year + '-' + month + '-' + day + ' ' + h
+}
